da95cb455bdd3e0085482cd6c2409f50437d4b26
Vasanthi1409 <158304622+Vasanthi1409@users.noreply.github.com>
2024-02-12 21:41:20 +0530

    Add files via upload

---
+++ Primitive_datatypes.java
@@ -0,0 +1,27 @@
+package Datatype.com;
+
+public class Primitive_datatypes {
+
+	public static void main(String[] args) {
+		byte a = 10;
+		System.out.println("The value of a is: " + a);
+		short b = 100;
+		System.out.println("The value of b is: " + b);
+		int c = 1000;
+		System.out.println("The value of c is: " + c);
+		long d = 10000l;
+		System.out.println("The value of d is: " + d);
+		float e = 543.123f;
+		System.out.println("The fvalue of e is: " + e);
+		double f = 1234.2000d;
+		System.out.println("The value of f is: " + f);
+		boolean g = true;
+		System.out.println(g);
+		boolean h = false;
+		System.out.println(h);
+		char i = 'T';
+		System.out.println(i);
+
+	}
+
+}
